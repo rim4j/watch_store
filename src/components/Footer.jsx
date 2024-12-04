a2370bdd09d6727134bf9d4083b71c27156c2e45
rim4j <amirhosseinjalali818@gmail.com>
2024-12-04 10:15:17 +0330

    completed footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,67 @@
 import styled from "styled-components";
 import logo from "./../assets/png/main_logo.png";
 import { AppStrings } from "../utils/strings";
+import { GoDotFill } from "react-icons/go";
+import FooterItem from "./FooterItem";
+import { MdOutlinePhoneEnabled } from "react-icons/md";
+import { FaHeadphones } from "react-icons/fa6";
+import { CgMail } from "react-icons/cg";
+import eNamad from "./../assets/png/logo-enamad-1.png";
+
+const col1 = [
+  {
+    title: "رهگیری سفارش",
+    icon: <GoDotFill color='#ff4156' size='12px' />,
+  },
+  {
+    title: "نحوه‌ی ارسال  ",
+    icon: <GoDotFill color='#ff4156' size='12px' />,
+  },
+  {
+    title: "قوانین  فروشگاه",
+    icon: <GoDotFill color='#ff4156' size='12px' />,
+  },
+  {
+    title: "سوالات پر تکرار",
+    icon: <GoDotFill color='#ff4156' size='12px' />,
+  },
+  {
+    title: "درباره‌ی فروشگاه",
+    icon: <GoDotFill color='#ff4156' size='12px' />,
+  },
+  {
+    title: "لیست برندگان",
+    icon: <GoDotFill color='#ff4156' size='12px' />,
+  },
+  {
+    title: "بلاگ",
+    icon: <GoDotFill color='#ff4156' size='12px' />,
+  },
+];
+
+const col2 = [
+  {
+    title: "09333333333",
+    icon: <MdOutlinePhoneEnabled color='#ff4156' size='18px' />,
+  },
+  {
+    title: "09988888888",
+    icon: <MdOutlinePhoneEnabled color='#ff4156' size='18px' />,
+  },
+  {
+    title: "0212222222",
+    icon: <FaHeadphones color='#ff4156' size='18px' />,
+  },
+  {
+    title: "www.watchstore@gmail.com",
+    icon: <CgMail color='#ff4156' size='18px' />,
+  },
+];
 
 const Footer = () => {
   return (
     <Wrapper>
-      <Container className='container'>
+      <Container>
         <div>
           <Logo>
             <img src={logo} alt='' />
@@ -22,9 +78,22 @@ const Footer = () => {
             <p className='time-shop'>{AppStrings.timeShop}</p>
           </div>
         </div>
-        <div>col</div>
-        <div>col2</div>
-        <div>namad</div>
+        <div className='footer-col'>
+          <h1>دسترسی سریع</h1>
+          {col1.map((item, i) => (
+            <FooterItem key={i} title={item.title} icon={item.icon} />
+          ))}
+        </div>
+        <div className='footer-col'>
+          <h1>راه های ارتباطی</h1>
+          {col2.map((item, i) => (
+            <FooterItem key={i} title={item.title} icon={item.icon} />
+          ))}
+        </div>
+        <div className='footer-e-namad'>
+          <h1>نماد ها</h1>
+          <img src={eNamad} alt='' />
+        </div>
       </Container>
     </Wrapper>
   );
@@ -33,6 +102,8 @@ const Footer = () => {
 const Wrapper = styled.div`
   background: linear-gradient(#e6e6e6, #fff);
   height: 200px;
+
+  padding: 4rem;
 
   .details-watch {
     font-size: 14px;
@@ -43,6 +114,7 @@ const Wrapper = styled.div`
   }
   .time-shop {
     font-size: 12px;
+    margin-bottom: 20px;
   }
 `;
 
@@ -50,6 +122,25 @@ const Container = styled.div`
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   gap: 4rem;
+  .footer-col {
+    margin: 20px;
+    h1 {
+      font-size: 16px;
+      color: #000;
+    }
+    p {
+      font-size: 14px;
+      padding: 10px 0;
+    }
+  }
+  .footer-e-namad {
+    margin: 20px;
+    h1 {
+      font-size: 16px;
+      color: #000;
+    }
+    text-align: center;
+  }
 `;
 
 const Logo = styled.div`
---
+++ src/components/FooterItem.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+const FooterItem = ({ title, icon }) => {
+  return (
+    <Container>
+      <span>{icon}</span>
+      <p>{title}</p>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  transition: all ease 0.3s;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  p {
+    margin-right: 10px;
+  }
+
+  &:hover {
+    p {
+      color: #ff4156;
+      transition: all ease 0.3s;
+    }
+    span {
+      margin-left: 5px;
+      transition: all ease 0.3s;
+    }
+  }
+`;
+
+export default FooterItem;
